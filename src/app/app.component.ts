5a77966e73fe79404c01dc0d7a3e2c5d05a37430
amazurek <sonsiad83@wp.pl>
2022-05-16 16:28:17 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,17 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   kod:string = "BEL 2-01";
   nazwa:string = "Bełchatów B01";
+  kodentsoe:string = "19W000000000017D";
+  typ:string = "Elektrownia cieplna";
+  paliwo:string = "węgiel brunatny";
+  kodElektrowni = "BEL";
+  nawzaElektrowni = "BEL";
+  kodEntsoweElektrowni = "19W000000000016F";
+  adresElektrowni = "";
+  gps = "";
+
+
+
 
   name = 'Angular ' + VERSION.major;
 }
