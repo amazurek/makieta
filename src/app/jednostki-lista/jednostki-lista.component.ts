1584a3fc240cabd72ed016a8132f9001a39536f6
amazurek <sonsiad83@wp.pl>
2022-06-01 14:30:35 +0200

    3 files modified, 2 added

---
+++ src/app/jednostki-lista/jednostki-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jednostki-lista',
+  templateUrl: './jednostki-lista.component.html',
+  styleUrls: ['./jednostki-lista.component.css']
+})
+export class JednostkiListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
