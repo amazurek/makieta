11685e7f5bad413b172ec9790cd524e8112b699e
amazurek <sonsiad83@wp.pl>
2022-06-07 11:11:01 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; 
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -9,7 +10,7 @@ import { MagazynyComponent } from './magazyny/magazyny.component';
 import { JednostkiListaComponent } from './jednostki-lista/jednostki-lista.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppR],
   declarations: [AppComponent, HelloComponent, JednostkiComponent, MagazynyComponent, JednostkiListaComponent],
   bootstrap: [AppComponent],
 })
