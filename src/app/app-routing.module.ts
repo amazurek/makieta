2fb92dc2abbd444aba9087cfd1eb0109abebcb28
amazurek <sonsiad83@wp.pl>
2022-06-07 12:12:11 +0200

    3 files modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JednostkiListaComponent } from './jednostki-lista/jednostki-lista.component';
 import { MagazynyComponent } from './magazyny/magazyny.component';
+import { ScenariuszeComponent } from './scenariusze/scenariusze.component';
 
 const routes: Routes = [
   {path:'', component: MagazynyComponent},
-  {path:'jednostki', component: }
+  {path:'jednostkiLista', component: JednostkiListaComponent},
+  {path:'scenariusze', component:ScenariuszeComponent}
 ];
 
 @NgModule({
---
+++ src/app/scenariusze/scenariusze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scenariusze',
+  templateUrl: './scenariusze.component.html',
+  styleUrls: ['./scenariusze.component.css']
+})
+export class ScenariuszeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
